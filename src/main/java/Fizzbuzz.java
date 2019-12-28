4c9566e2ebf8401364aa9acf33f01a4221b620a2
GeorgeWuuu <547402765@qq.com>
2019-12-28 13:31:17 +0800

    TDD练习

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -3,4 +3,25 @@ public class Fizzbuzz {
     public String name(){
         return "Hello World";
     }
+
+    public static String of(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+
+        return String.valueOf(number);
+    }
+
 }
